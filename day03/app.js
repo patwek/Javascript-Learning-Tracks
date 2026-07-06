98c23afbab205c636c9bf9d23f6f531be557c96b
jdoe <jdoe83779@gmail.com>
2026-07-06 16:06:19 +0800

    Day 3 Learning Operators

---
+++ day03/app.js
@@ -0,0 +1,107 @@
+/*
+Arithmetic
+
+let a = 10;
+let b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);    
+
+
+Comparison
+
+let age = 25;
+
+console.log(age > 18);
+console.log(age < 18);
+console.log(age == 25);
+console.log(age !== 30);
+
+
+&& or AND
+let age = 22;
+let hasID = true;
+
+console.log(age >= 18 && hasID);
+&& or AND
+
+|| or OR
+let hasStudentID = false;
+let hasDriversLicense = true;
+
+console.log(hasStudentID || hasDriversLicense);
+|| or OR
+
+! or NOT 
+let loggedIn = false;
+
+console.log(!loggedIn);
+
+
+
+let num1 = 20;
+let num2 = 5;
+
+console.log("Addition: ", num1 + num2);
+console.log("Subtraction: ", num1 - num2);
+console.log("Multiplication: ", num1 * num2);
+console.log("Division: ", num1 / num2);
+
+
+const num1 = 12;
+const num2 = 4;
+
+console.log(`${num1} + ${num2} = ${num1 + num2}`);
+console.log(`${num1} - ${num2} = ${num1 - num2}`);
+console.log(`${num1} * ${num2} = ${num1 * num2}`);
+console.log(`${num1} / ${num2} = ${num1 / num2}`);
+
+
+let weight = 70;
+let height = 1.75;
+
+const bmi = weight / (height ** 2);
+
+console.log("Weight", weight);
+console.log("Height: ", height);
+console.log("BMI: ", bmi);
+
+
+
+let base = 10;
+let height = 6;
+
+const area = (base * height) / 2;
+
+console.log("The Base Value is: ", base);
+console.log("The height Value is: ", height);
+console.log("The area of the Rectangle is: ", area);
+
+
+
+let num1 = 100;
+let num2 = 9;
+
+console.log(num1 % num2);
+
+
+let age = 18;
+
+if (age >= 17) {
+    console.log("Not Eligible to Vote");
+} else {
+    console.log("Eligible to Vote");
+}
+
+*/
+let num1 = 59;
+
+if (num1 <= 100) {
+    console.log("In between 1 to 100");
+} else {
+    console.log("It is more than");
+}
